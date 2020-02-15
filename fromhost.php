f52b51ab877c24bc3df82b5662392995fa122156
Steve Boyd <emteknetnz@gmail.com>
2020-02-16 11:07:46 +1300

    Create todo list

---
+++ fromhost.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+- host machine calls this script
+- will propably be linked to from vendor/bin/legion
+- will spin up container a
+- this may include building/downloading image form dockerhub
+- will run /dev/build?flush on container a
+- this step will be potentially very slow, so need messaging to set expectations with user
+*/
+
+
