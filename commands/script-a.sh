077f187e4896d93e261f7a9a7bd0404770a42788
Steve Boyd <emteknetnz@gmail.com>
2020-02-15 23:07:50 +1300

    Move code to dockerfile

--- commands/script-a.sh
+++ commands/script-a.sh
@@ -1,13 +1,10 @@
 # create a new group and add www-data (apache user) to it
 # we do this is because we're using a shared folder
-# TODO: possibly if [ ! group exists vboxsf ] so that I can reboot this container
-echo "Handling shared folder permission"
-groupadd -g 998 myvboxsf
-usermod -aG myvboxsf www-data
+# # TODO: possibly if [ ! group exists vboxsf ] so that I can reboot this container
+# echo "Handling shared folder permission"
+# groupadd -g 998 myvboxsf
+# usermod -aG myvboxsf www-data
 
-# set identifier file used by dockerenv/bootstrap.php
-echo "Adding is_legion_docker.txt file into /home"
-touch /home/is_legion_docker.txt
 
 # TODO: move this stuff to webserver dockerfile
 
